d838c1beb085c2e9734f2ae4054f4fb908909f93
Angel <angel.nieva@usach.cl>
2023-07-22 21:54:24 -0400

    a

--- pregunta-service/src/main/java/com/mingeso/preguntas/PreguntaServiceApplication.java
+++ pregunta-service/src/main/java/com/mingeso/preguntas/PreguntaServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PreguntasServiceApplication {
+public class PreguntaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PreguntasServiceApplication.class, args);
+		SpringApplication.run(PreguntaServiceApplication.class, args);
 	}
 
 }
